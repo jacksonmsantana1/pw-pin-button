4a79d375bf5c0e5a8775e9663e967a4b8ab6e5f9
Jackson Martorano Sant'Ana <jackson.m.santana1@gmail.com>
2016-07-26 20:30:23 -0300

    Removed the IO pkg and installed it with npm

--- src/pw-pin-button/pw-pin-button.js
+++ src/pw-pin-button/pw-pin-button.js
@@ -6,7 +6,7 @@ import R from 'ramda';
 import Maybe from 'data.maybe';
 import Either from 'data.either';
 import TypeSafety from 'typesafety';
-import IO from '../lib/IO/IO';
+import IO from 'io-monad';
 import Helpers from '../lib/Helpers/Helpers';
 import ClassList from '../lib/ClassList/ClassList';
 import HTMLFunctional from '../lib/HTMLFunctional/HTMLFunctional';
